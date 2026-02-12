4d95eef9c18fb77a9052c56d49fac38ed8fb4d35
ColleFALL <collefall118@gmail.com>
2026-02-12 09:21:34 +0000

    api paths create corriger

--- paths/urls.py
+++ paths/urls.py
@@ -3,5 +3,7 @@ from paths.views.path_views import PathCreateView, PathListView
 
 urlpatterns = [
     path('paths/', PathListView.as_view(), name='path-list'),
-    path('paths/', PathCreateView.as_view(), name='path-create'),
+    path('paths/create/', PathCreateView.as_view(), name='path-create'),
 ]
+
+
