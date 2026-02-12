15ce87d378e6e0ae4f2833f64d83071c609ea5a2
ColleFALL <collefall118@gmail.com>
2026-02-12 15:18:48 +0000

    recuperation des chemins

--- paths/urls.py
+++ paths/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from paths.views.path_views import PathCreateView, PathListView
+from paths.views.path_views import PathCreateView, PathListView, PathDetailView
 
 urlpatterns = [
     path('paths/', PathListView.as_view(), name='path-list'),
     path('paths/create/', PathCreateView.as_view(), name='path-create'),
+    path('paths/<int:pk>/', PathDetailView.as_view(), name='path-detail'),  # <- à ajouter
 ]
 
 
