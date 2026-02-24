6e272d01320ed0e45796d639a748b829585a05fb
ColleFALL <collefall118@gmail.com>
2026-02-24 09:27:06 +0000

    debugs auth admin3

--- admin_panel/permissions.py
+++ admin_panel/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework.permissions import BasePermission
 
 class IsAdminRole(BasePermission):
-    """
-    Autorise uniquement les utilisateurs ayant role='admin'
-    """
-
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated and
-            getattr(request.user, "role", None) == "admin"
+            (
+                request.user.role == "admin"
+                or request.user.is_staff
+                or request.user.is_superuser
+            )
         )
