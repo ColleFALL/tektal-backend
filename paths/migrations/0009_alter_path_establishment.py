7996b8bc4eacdb699a1292201aea3bf682115929
ColleFALL <collefall118@gmail.com>
2026-03-03 16:06:04 +0000

    Fix migrations and establishment field2

--- paths/migrations/0009_alter_path_establishment.py
+++ paths/migrations/0009_alter_path_establishment.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='path',
             name='establishment',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='paths', to='paths.establishment'),
