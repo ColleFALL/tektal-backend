7510ba97a46adc35efe9ebe6d89a295d84e1ab01
ColleFALL <collefall118@gmail.com>
2026-03-05 09:07:28 +0000

    recuperation du travaille d etablissement1

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,35 +1,3 @@
-
-# from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
-# from djoser.serializers import UserSerializer as BaseUserSerializer
-# from django.contrib.auth import get_user_model
-
-# User = get_user_model()
-
-
-# class UserCreateSerializer(BaseUserCreateSerializer):
-#     """Création utilisateur (name + email + password)"""
-
-#     class Meta(BaseUserCreateSerializer.Meta):
-#         model = User
-#         fields = ("id", "email", "name", "password", "role")
-#         extra_kwargs = {
-#             "password": {"write_only": True},
-#             "name": {"required": False},
-#             "role": {"required": False},
-#         }
-
-#     def create(self, validated_data):
-#         validated_data["role"] = "participant"
-#         return User.objects.create_user(**validated_data)
-
-
-# class UserSerializer(BaseUserSerializer):
-#     """Retour profil utilisateur"""
-
-#     class Meta(BaseUserSerializer.Meta):
-#         model = User
-#         fields = ("id", "email", "name", "role", "is_active", "date_joined")
-#         read_only_fields = ("id", "is_active", "date_joined")
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from django.contrib.auth import get_user_model
@@ -65,7 +33,7 @@ class UserSerializer(BaseUserSerializer):
         read_only_fields = ("id", "is_active", "date_joined")
 
     def get_establishment_name(self, obj):
-    try:
-        return obj.establishment.name  # related_name = 'establishment' dans paths/models.py
-    except:
-        return None
+        try:
+            return obj.etablissement.name
+        except:
+            return None
