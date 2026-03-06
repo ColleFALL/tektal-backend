e9913d427675e4c6b62342af715c577084f95fe6
ColleFALL <collefall118@gmail.com>
2026-03-06 12:02:42 +0000

    recuperation du travaille d etablissement14

--- paths/serializers/saved_path_serializer.py
+++ paths/serializers/saved_path_serializer.py
@@ -1,8 +1,11 @@
 # paths/serializers/saved_path_serializer.py
 from rest_framework import serializers
 from paths.models import SavedPath
+from paths.serializers.path_serializer import PathSerializer  # import votre serializer existant
 
 class SavedPathSerializer(serializers.ModelSerializer):
+    path = PathSerializer(read_only=True)  # ✅ retourne l'objet complet
+
     class Meta:
         model = SavedPath
         fields = ['id', 'user', 'path', 'created_at']
